922b7eaad5fc60cdc147423e1cb5c69f1f7032d5
serkan <sserkanakin@icloud.com>
2025-10-19 15:50:55 +0200

    changes

--- synthesis-modified/paynt/examples.py
+++
@@ -1,5 +0,0 @@
-from . import version
-
-import os
-
-paynt_models_dir = os.path.join(os.path.dirname(__file__), "../models/")
--- synthesis-modified/payntbind/src/synthesis/smg/bindings.cpp
+++
@@ -1,7 +0,0 @@
-#include "../synthesis.h"
-
-#include "smgModelChecker.h"
-
-void bindings_smg(py::module& m) {
-    m.def("model_check_smg", &synthesis::modelCheckSmg<double>, py::arg("smg"), py::arg("formula"), py::arg("only_initial_states") = false, py::arg("set_produce_schedulers") = true, py::arg("env") = storm::Environment() );
-}
